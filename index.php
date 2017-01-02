22ea39f961c4b5d07059fe8f07cc37b0246bff34
Pascal <Pascal@MKC-STATION>
2017-01-02 14:55:34 +0100

    adopt php

--- index.php
+++ index.php
@@ -1,1 +1,188 @@
-<?php include_once("home.html"); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pascal Sarrazin - Curriculum Vitae</title>
+
+        <meta name="viewport" content="width=device-width"/>
+        <meta name="description" content="Le Curriculum Vitae de Pascal SARRAZIN."/>
+        <meta charset="UTF-8"> 
+
+        <link type="text/css" rel="stylesheet" href="style.css">
+        <link href='http://fonts.googleapis.com/css?family=Rokkitt:400,700|Lato:400,300' rel='stylesheet' type='text/css'>
+
+        <!--[if lt IE 9]>
+        <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <script>
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+            ga('create', 'UA-58252507-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+    </head>
+    <body id="top">
+        <div id="cv" class="instaFade">
+            <div class="mainDetails">
+                <div id="headshot">
+                    <img src="headshot.jpg" alt="Pascal Sarrazin" />
+                </div>
+
+                <div id="name">
+                    <h1 class="quickFade delayOne">Pascal Sarrazin</h1>
+                    <h2 class="quickFade delayTwo">Manager</h2>
+                </div>
+
+                <div id="contactDetails" class="quickFade delayThree">
+                    <ul>
+                        <li>email: <a href="mailto:contact-nospam@pascalsarrazin.fr" target="_blank">contact(at)pascalsarrazin.fr</a></li>
+                        <li>web: <a href="http://www.pascalsarrazin.fr">www.pascalsarrazin.fr</a></li>
+                        <li>&phone;: 07.52.03.23.14</li>
+                        <li class="addresspost">adr: 900 chemin de Savy</li>
+                        <li class="addresspost">&emsp;&emsp; 26250 Livron sur Drome</li>
+                    </ul>
+                </div>
+                <div class="clear"></div>
+            </div>
+
+            <div id="mainArea" class="quickFade delayFour">
+                <section>
+                    <article>
+                        <div class="sectionTitle">
+                            <h1>Profil</h1>
+                        </div>
+
+                        <div id="profile" class="sectionContent">
+                            <p>Manager agilo-pragmatique, c’est sur de solides bases d’ingénieur et de développeur logiciel embarqué que je construis depuis plusieurs années mon expérience en management.</p>
+                            <p>Construire des équipes, les faire progresser constituent mes missions et surtout mes principales motivations.</p>
+                        </div>
+                    </article>
+                    <div class="clear"></div>
+                </section>
+
+
+                <section>
+                    <div class="sectionTitle">
+                        <h1>Expériences</h1>
+
+                    </div>
+
+                    <div class="sectionContent">
+                        <article>
+                            <h2>Manager R&D / Software discipline EMEA - Markem-Imaje / Valence</h2>
+                            <p class="subDetails">Janvier 2014 - Présent</p>
+                            <p>Structurer une organisation d’environ 25 développeurs logiciels et 3 managers repartis entre la France, l’Angleterre, la Belgique et l’Espagne pour améliorer l’efficacité du département. Renforcement de la collaboration entre les équipes, mise en place de stratégies de partenariat, de pratiques de développement et de management agiles</p>
+                            <p class="competences">Définition et stratégie d’organisation, Mangement de manager, Leadership, Management interculturel.</p>
+                        </article>
+
+                        <article>
+                            <h2>Product Platform Engineering Manager - Markem-Imaje</h2>
+                            <p class="subDetails">Janvier 2008 - Décembre 2013</p>
+                            <p>Manager une équipe de 6 à 8 ingénieurs pour développer les plateformes embarquées utilisées par le groupe. En collaboration avec les autres sites de développement et un directeur basé aux états unis.</p>
+                            <p class="competences">Management, collaboration international (US, Suède, Inde, UK), Déploiements pratiques agiles.</p>
+                        </article>
+
+                        <article>
+                            <h2>Responsable technique - Imaje</h2>
+                            <p class="subDetails">Juillet 2004 - Décember 2008</p>
+                            <p>Responsable technique développement logiciel embarqué. Coordination d’une équipe de 3 ingénieurs pour développer les logiciels embarqués de 2 gammes d’imprimantes.</p>
+                            <p class="competences">Leadership technique et architecture système. Coordination et gestion de projets</p>
+                        </article>
+
+                        <article>
+                            <h2>Ingénieur développement logiciel embarqué - Imaje / Valence</h2>
+                            <p class="subDetails">Juin 1999 - Juin 2004</p>
+                            <p>Développements variés (temp réel, IHM, traitement de données, protocols) sur divers gammes d’imprimantes industrielles.</p>
+                            <p class="competences">Programation C/C++/C#, RTOS, WinCE. Projets industriels multidisciplinaires</p>
+                        </article>
+
+                        <article>
+                            <h2>Ingénieur développement logiciel - ALSI / Valence</h2>
+                            <p class="subDetails">Juillet 1997 - Juin 1999</p>
+                            <p>Développement de systèmes de gestion des dispositifs de comptage (depuis les terminaux portables jusqu'aux systèmes de gestion). Programmation en C/C++ Pascal, base de données Oracle.</p>
+                            <p class="competences">Formation des utilisateurs, support avant/après vente, installation sur site.</p>
+                        </article>
+
+                        <article>
+                            <h2>Ingénieur développement logiciel -Napac / Paris</h2>
+                            <p class="subDetails">Janvier 1995 - Juin 1997</p>
+                            <p>Développement pour des équipements de télégestion. C++ sous Win16 et Win32.</p>
+                            <p class="competences">Multitache et protocol de communications (bus de terrain) en environnement Windows</p>
+                        </article>
+                    </div>
+                    <div class="clear"></div>
+                </section>
+
+
+                <section>
+                    <div class="sectionTitle">
+                        <h1>Compétences clés</h1>
+                    </div>
+
+                    <div class="sectionContent">
+                        <ul class="keySkills">
+                            <li>Management de manager</li>
+                            <li>Management d’équipes y compris multiculturelles</li>
+                            <li>Management de projets</li>
+                            <li>Scrum Master (CSM Scrum Alliance)</li>
+
+                            <li>Approche Agile pour les projets (SAFe, Scrum, Kanban)</li>
+                            <li>Approche Agile dans le management (Management 3.0)</li>
+                            <li>Architecture sociale (développement d’équipes)</li>
+
+                            <li>Architecture et intégration systèmes</li>
+                            <li>Systèmes embarqués et temps réels</li>
+                            <li>Développement logiciel (C/C++/C# et autres)</li>
+                        </ul>
+                    </div>
+                    <div class="clear"></div>
+                </section>
+
+
+                <section>
+                    <div class="sectionTitle">
+                        <h1>Formations académiques</h1>
+                    </div>
+
+                    <div class="sectionContent">
+                        <article>
+                            <h2>Ecole Supérieure d'Electronique de l'Ouest (ESEO) - Angers/France</h2>
+                            <p class="subDetails">1989-1994 promotion Gauss</p>
+                            <p>Diplôme d'Ingenieur en Electronique, automatique et informatique industrielle</p>
+                        </article>
+
+                        <article>
+                            <h2>Lycée Saint Joseph - La Roche sur Yon/France</h2>
+                            <p class="subDetails">1986-1989</p>
+                            <p>Baccalauréat série C -mention AB</p>
+                        </article>
+                    </div>
+                    <div class="clear"></div>
+                </section>
+ 
+                <section>
+                    <div class="sectionTitle">
+                        <h1>Activités autres</h1>
+                    </div>
+
+                    <div class="sectionContent">
+                        <ul class="keySkills">
+                            <li>Animateur de l'antenne Valentinoise du CARA (Club Agile Rhone Alpes)</li>
+                            <li>Pratique de la randonnée en montagne</li>
+                        </ul>
+                    </div>
+                    <div class="clear"></div>
+                </section>
+
+            </div>
+        </div>
+    </body>
+</html>
