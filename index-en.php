bbfc93648aa4cfb4d8350b137254449374000ccb
Pascal <Pascal@MKC-STATION>
2017-07-05 22:33:52 +0200

    added english version (not deploy)

---
+++ index-en.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pascal Sarrazin - Curriculum Vitae</title>
+
+        <meta name="viewport" content="width=device-width"/>
+        <meta name="description" content="Le Curriculum Vitae de Pascal SARRAZIN."/>
+        <meta charset="UTF-8"> 
+
+        <link type="text/css" rel="stylesheet" href="style.css">
+        <link href='http://fonts.googleapis.com/css?family=Rokkitt:400,700|Lato:400,300' rel='stylesheet' type='text/css'>
+
+        <!--[if lt IE 9]>
+        <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <script>
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+            ga('create', 'UA-58252507-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+    </head>
+    <body id="top">
+        <div id="cv" class="instaFade">
+            <div class="mainDetails">
+                <div id="headshot">
+                    <img src="headshot.jpg" alt="Pascal Sarrazin" />
+                </div>
+
+                <div id="name">
+                    <h1 class="quickFade delayOne">Pascal Sarrazin</h1>
+                    <h2 class="quickFade delayTwo">Manager</h2>
+                    <h2 class="quickFade delayTwo">of R&D Department</h2>
+                    <h3 class="quickFade delayTwo">birth 15-04-1971, 46 years </h3>
+                    <h3 class="quickFade delayTwo">Maried, 2 childs </h3>
+                </div>
+
+                <div id="contactDetails" class="quickFade delayThree">
+                    <ul>
+                        <li>email: <a href="mailto:contact-nospam@pascalsarrazin.fr" target="_blank">contact(at)pascalsarrazin.fr</a></li>
+                        <li>web: <a href="http://www.pascalsarrazin.fr">www.pascalsarrazin.fr</a></li>
+                        <li>&phone;: +33 7.52.03.23.14</li>
+                        <li class="addresspost">adr: 900 chemin de Savy</li>
+                        <li class="addresspost">&emsp;&emsp; F-26250 Livron sur Drome (FRANCE)</li>
+                    </ul>
+                </div>
+                <div class="clear"></div>
+            </div>
+
+            <div id="mainArea" class="quickFade delayFour">
+                <section>
+                    <article>
+                        <div class="sectionTitle">
+                            <h1>Profile</h1>
+                        </div>
+
+                        <div id="profile" class="sectionContent">
+                            <p>Agilo-pragmatic manager, it’s on deep engineer and embedded software developer roots that I’m building a strong experience in management from years.</p>
+                            <p>Building teams, making them grow are my missions but also my main motivations.</p>
+                        </div>
+                    </article>
+                    <div class="clear"></div>
+                </section>
+
+
+                <section>
+                    <div class="sectionTitle">
+                        <h1>Experience</h1>
+
+                    </div>
+
+                    <div class="sectionContent">
+                        <article>
+                            <h2>Manager R&D / System Integration - Markem-Imaje / Valence</h2>
+                            <p class="subDetails">June 2016 - Present</p>
+                            <p>Coordinate and anticipate system integration, with a multidisciplinary team (5 members) to support deployment of an agile at scale approach for a ambitious program (involving about environ 80 persons and based on a SAFe framework implementation). Create synergies across development and system architects teams by deploying an integration infrastructure from mechanical system to embedded software.</p>
+                            <p class="competences">Multidisciplinary program coordination, Agile at scale (SAFe) , Platforming strategy and system integration. </p>
+                        </article>
+                        
+                        <article>
+                            <h2>Manager R&D / Software discipline EMEA - Markem-Imaje / Valence</h2>
+                            <p class="subDetails">January 2014 - June 2016</p>
+                            <p>Structure an organization of about 25 developers and 3 managers split across France, Belgium, UK and Spain to improve overall efficiency. Reinforce collaboration among teams, deploy partnerships, and agile development and management practices</p>
+                            <p class="competences">Organization definition and strategy, Manage Managers, Leadership, Intercultural Management.</p>
+                        </article>
+
+                        <article>
+                            <h2>Product Platform Engineering Manager - Markem-Imaje</h2>
+                            <p class="subDetails">January 2008 - December 2013</p>
+                            <p>Manage a team of 6 to 8 engineers to develop and maintain embedded platform and associated systems, in collaboration with several other development sites.</p>
+                            <p class="competences">Management, International project and collaboration (USA, Sweden, India, UK), Agile practices deployment (Scrum/Kanban).</p>
+                        </article>
+
+                        <article>
+                            <h2>Tech Lead - Imaje</h2>
+                            <p class="subDetails">July 2004 - December 2008</p>
+                            <p>Embedded software tech lead. Coordination of a team of 3 engineers to develop embedded software for several industrial printer ranges</p>
+                            <p class="competences">Technical Leadership, system architecture (UML/SysML). Project management.</p>
+                        </article>
+
+                        <article>
+                            <h2>Embedded Software Engineer - Imaje / Valence</h2>
+                            <p class="subDetails">June 1999 - June 2004</p>
+                            <p>Various embedded development (real time, HMI, data management) on several industrial printer ranges.</p>
+                            <p class="competences">C/C++/C# languages, RTOS, WinCE. Industrial multidisciplinary project.</p>
+                        </article>
+
+                        <article>
+                            <h2>Software Engineer - ALSI / Valence</h2>
+                            <p class="subDetails">July 1997 - June 1999</p>
+                            <p>Development of counting systems (for energy, water distribution) from hand held terminal to main frame system. C/C++ Pascal programming, Oracle Data base management.</p>
+                            <p class="competences">End user training, pre and post sales technical support, on site deployment.</p>
+                        </article>
+
+                        <article>
+                            <h2>Software Engineer -Napac / Paris</h2>
+                            <p class="subDetails">January 1995 - June 1997</p>
+                            <p>Software development of Remote monitoring equipment .</p>
+                            <p class="competences">Telecommunication protocol, Field Bus, Multitasking on Windows environment</p>
+                        </article>
+                    </div>
+                    <div class="clear"></div>
+                </section>
+
+
+                <section>
+                    <div class="sectionTitle">
+                        <h1>Keys skills and competences</h1>
+                    </div>
+
+                    <div class="sectionContent">
+                        <ul class="keySkills">
+                            <li>Manage managers</li>
+                            <li>Manage team including in a multicultural environment</li>
+                            <li>Project management</li>
+                            <li>Scrum Master (CSM Scrum Alliance)</li>
+
+                            <li>Agile project management (SAFe, Scrum, Kanban)</li>
+                            <li>Agile management (Management 3.0)</li>
+                            <li>Social Architecture (Team creation and growth)</li>
+
+                            <li>System architecture and integration (industrial)</li>
+                            <li>Embedded and Real Time System</li>
+                            <li>Software development (mainly C++/C#)</li>
+                        </ul>
+                    </div>
+                    <div class="clear"></div>
+                </section>
+
+
+                <section>
+                    <div class="sectionTitle">
+                        <h1>Qualifications</h1>
+                    </div>
+
+                    <div class="sectionContent">
+                        <article>
+                            <h2>Ecole Supérieure d'Electronique de l'Ouest (ESEO) - Angers/France</h2>
+                            <p class="subDetails">1989-1994 promotion Gauss</p>
+                            <p>Master in Engineering : Computer sciences, electronics and automation </p>
+                        </article>
+
+                        <article>
+                            <h2>Lycée Saint Joseph - La Roche sur Yon/France</h2>
+                            <p class="subDetails">1986-1989</p>
+                            <p>Baccalauréat série C -mention AB</p>
+                        </article>
+                    </div>
+                    <div class="clear"></div>
+                </section>
+ 
+                <section>
+                    <div class="sectionTitle">
+                        <h1>Other activities</h1>
+                    </div>
+
+                    <div class="sectionContent">
+                        <ul class="keySkills">
+                            <!--Animateur de l'antenne <a href="http://groupspaces.com/CARAValence">Valentinoise du CARA</a> (<a href="http://www.clubagilerhonealpes.org">Club Agile Rhone Alpes)</a>-->
+                            <li>Animation of the local branch of a developpers club sharing software and agile practices</li>
+                            <li>French Tech in the Alps Valence Romans board member (tech community hub)</li>
+                            <li>Mountain hicking, swimming</li>
+                        </ul>
+                    </div>
+                    <div class="clear"></div>
+                </section>
+            </div>
+        </div>
+    </body>
+</html>
